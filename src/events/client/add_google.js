930977a87417aa2165100247b04520ae8d5bf460
happyandy1121 <andymao20021121@gmail.com>
2023-05-08 10:54:17 +0800

    新增多國語言聊天室的自動添加表情(通識課專用)

---
+++ src/events/client/add_google.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'messageCreate',
+    async execute(client) {
+        if (client.channel.id == '1089092910676443136') {
+            client.react("<:tran:1056423371551809618>")
+        };
+    }
+}
--- src/events/client/msg.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'messageCreate',
-    async execute(client) {
-        if (client.author.id == '1074986819214643330') return;
-        //client.reply("aa")
-    }
-}
