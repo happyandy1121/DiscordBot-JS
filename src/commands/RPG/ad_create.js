e8c93f8258b22313dc399e58dc0e55bdfda3aebe
happyandy1121 <andymao20021121@gmail.com>
2024-01-11 09:28:42 +0800

    新增RPG管理員指令

---
+++ src/commands/RPG/ad_create.js
@@ -0,0 +1,84 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, TextInputBuilder, TextInputStyle, EmbedBuilder, Embed } = require('discord.js');
+const Member = require('../../models/member')
+const Exp = require('../../models/exp')
+const { expAdd } = require('../../../data/RPG/exp')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('create')
+        .setDescription('for admin')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('exp')
+                .setDescription('manage exp')
+                .addStringOption(option =>
+                    option.setName('work')
+                        .setDescription('choose')
+                        .setRequired(true)
+                        .addChoices(
+                            { name: 'farm', value: 'farmLevel' },
+                            { name: 'mine', value: 'mineLevel' },
+                            { name: 'log', value: 'logLevel' },
+                            { name: 'fish', value: 'fishLevel' },
+                            { name: 'make', value: 'makeLevel' },
+                        ) 
+                )
+                .addStringOption(option =>
+                    option.setName('num')
+                        .setDescription('give')
+                        .setRequired(true)
+                )
+                .addUserOption(option =>
+                    option.setName('player')
+                        .setDescription('choose')
+                        .setRequired(true)
+                ),
+        ),
+        
+    async execute(interaction, client) {
+        try {
+            const work = interaction.options.getString('work');
+            const num = interaction.options.getString('num');
+            const result = interaction.options.getUser('player');
+            if (result.id !== '584315794012241933') return
+            const expUser = await Exp.findOne({ where: { id: result.id } })
+            if (!expUser) {
+                await interaction.reply({ content: '查無此玩家資料', ephemeral: true })
+                return
+            }
+
+            if (work) {
+                console.log(expAdd(expUser, work, num))
+                if (work === 'farmLevel') {
+                    await expUser.update({ farmLevel: expAdd(expUser, work, num) });
+                    await interaction.reply({ content: '經驗值更改成功' })
+                }
+                else if (work === 'mineLevel') {
+                    await expUser.update({ mineLevel: expAdd(expUser, work, num) });
+                    await interaction.reply({ content: '經驗值更改成功' })
+                }
+                else if (work === 'logLevel') {
+                    await expUser.update({ logLevel: expAdd(expUser, work, num) });
+                    await interaction.reply({ content: '經驗值更改成功' })
+                }
+                else if (work === 'fishLevel') {
+                    await expUser.update({ fishLevel: expAdd(expUser, work, num) });
+                    await interaction.reply({ content: '經驗值更改成功' })
+                }
+                else if (work === 'makeLevel') {
+                    await expUser.update({ makeLevel: expAdd(expUser, work, num) });
+                    await interaction.reply({ content: '經驗值更改成功' })
+                }
+                else {
+                    await interaction.reply({ content: '怪怪的' })
+                }
+            }
+
+        } catch (error) {
+            await interaction.reply({
+                content: `There is no code for this question.\nYour donate will accelerate production!`,
+                ephemeral: true
+            });
+        }
+    }
+}
